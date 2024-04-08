8487a36be434da1b36d5007e3d05ad37354f1fa6
Ye Jun <yejun.code@gmail.com>
2024-04-08 17:24:54 +0800

    Swap version extraction order in script
    This commit changes the order of version extraction in the extract_version.sh script. Previously, the script first tried to extract the version using the version_pattern, and if that failed, it tried to extract the version using the update_version_pattern.
    
    Now, the script first tries to extract the version using the update_version_pattern, and if that fails, it tries to extract the version using the version_pattern. This change ensures that the most recent version is extracted first, which is more likely to be the correct version.
    
    This change does not affect the functionality of the script, but it may improve the accuracy of the version extraction process.

--- extract_version.sh
+++ extract_version.sh
@@ -4,8 +4,8 @@ update_version_pattern="Update ([0-9]+\.[0-9]+\.[0-9]+)"
 version_pattern="\(version ([0-9].*)\)"
 html_content=$(curl -sL https://code.visualstudio.com/updates)
 
-version=$(echo "$html_content" | grep -oE "$version_pattern" | awk '{gsub(/\(version |\)/, ""); print $0}')
+version=$(echo "$html_content" | grep -oE "$update_version_pattern" | awk '{print $2}' | sort -rV | head -n 1)
 if [ -z "$version" ]; then
-    version=$(echo "$html_content" | grep -oE "$update_version_pattern" | awk '{print $2}' | sort -rV | head -n 1)
+    version=$(echo "$html_content" | grep -oE "$version_pattern" | awk '{gsub(/\(version |\)/, ""); print $0}')
 fi
 echo "$version"
